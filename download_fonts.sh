9a20f2f7abc1f23b82f1be218579d6b1411e24dc
--replace <tejumoladavid@gmail.com>
2024-04-03 23:58:39 -0400

    add command options

--- download_fonts.sh
+++ download_fonts.sh
@@ -2,13 +2,27 @@
 
 set -x
 
-REPO_URL="https://api.github.com/repos/fauxparse/circular-std/contents/fonts"
+REPO_URL="$1"
+FILE_EXTENSION="$2"
+DOWNLOAD_LOCATION="$3"
 
-# Loop through each file in the directory
-curl -s "$REPO_URL" | grep -oE '"download_url": "([^"]+\.ttf|[^"]+\.otf)"' | grep -oE 'https://[^"]+' | while read -r DOWNLOAD_URL; do
+if [ -z "$REPO_URL" ]; then
+    echo "Usage: $0 REPO_URL [FILE_EXTENSION]"
+    exit 1
+fi
+
+if [ -z "$FILE_EXTENSION" ]; then
+    FILE_EXTENSION=".ttf"
+fi
+
+# If DOWNLOAD_LOCATION is not provided, default to "/Library/Fonts/" - Installs font directly so you don't have to install manually with font book
+if [ -z "$DOWNLOAD_LOCATION" ]; then
+    DOWNLOAD_LOCATION="/Library/Fonts/"
+fi
+
+curl -s "$REPO_URL" | grep -oE '"download_url": "([^"]+\'$FILE_EXTENSION')"' | grep -oE 'https://[^"]+' | while read -r DOWNLOAD_URL; do
     FILENAME=$(basename "$DOWNLOAD_URL")
-    echo "$FILENAME"
-    # Download the font file to /Library/Fonts/
-    curl -s -o "/Library/Fonts/$FILENAME" "$DOWNLOAD_URL"
+
+    curl -s -o "${DOWNLOAD_LOCATION}${FILENAME}" "$DOWNLOAD_URL"
     echo "Downloaded $FILENAME"
 done
